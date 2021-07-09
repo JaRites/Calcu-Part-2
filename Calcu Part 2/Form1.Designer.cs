733fe50fe91b180bb1ee76fe3b490e5e0eb8c4b8
Kim Dahyun <ritesjarman@gmail.com>
2021-07-09 09:44:04 +0800

    Icon

--- Calcu Part 2/Form1.Designer.cs
+++ Calcu Part 2/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Calcu_Part_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BtnFraction = new System.Windows.Forms.Button();
             this.BtnSquare = new System.Windows.Forms.Button();
             this.BtnClear = new System.Windows.Forms.Button();
@@ -357,6 +358,7 @@ namespace Calcu_Part_2
             this.Controls.Add(this.BtnClear);
             this.Controls.Add(this.BtnSquare);
             this.Controls.Add(this.BtnFraction);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Calculator";
             this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
